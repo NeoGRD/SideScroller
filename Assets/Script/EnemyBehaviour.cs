1f237c33c9ed21cf879490fe0aaab3394c28fc06
NeoGRD <n.gerard@rns.ecolescreatives.com>
2026-03-29 23:56:37 +0200

    just a whole bunch of bullshit innit

--- Assets/Script/EnemyBehaviour.cs
+++ Assets/Script/EnemyBehaviour.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.UIElements;
 
 public class EnemyBehaviour : MonoBehaviour
@@ -10,16 +11,33 @@ public class EnemyBehaviour : MonoBehaviour
     public float bulletSpeed;
     public float rotationSpeed;
     public float shootSpeed;
+    //public bool canAim = false;
+    //public Transform targetTf;
+    // private bool isAiming;
 
 
     void Start()
     {
-        player = FindFirstObjectByType<PlayerMovementPlatformer>();
-        tf = GetComponent<Transform>();
+        player      = FindFirstObjectByType<PlayerMovementPlatformer>();
+        targetTf    = FindFirstObjectByType<PlayerMovementPlatformer>().transform;
+        tf          = GetComponent<Transform>();
     }
 
     void Update()
     {
-        tf.Rotate(0,0, rotationSpeed);
+        //if (isAiming)
+        //{
+        //    tf.Rotate(0,0, rotationSpeed);
+        //}
+        //else
+        //{
+        //   transform.LookAt(targetTf);
+        //}
     }
+
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    isAiming = true;
+    //}
+
 }
